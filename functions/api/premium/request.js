3754ed6b4b3b70acd50a55d20ede697770a40c64
text <rclothes67@gmail.com>
2026-04-20 18:16:56 +0100

    Delete functions directory

--- functions/api/premium/request.js
+++
@@ -1,7 +0,0 @@
-import { json } from "../../_lib/auth.js";
-
-export async function onRequestPost() {
-  return json({
-    error: "Manual transaction-hash premium claims are disabled. Premium now activates automatically when a matching payment is detected.",
-  }, { status: 410 });
-}
